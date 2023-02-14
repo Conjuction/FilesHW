a3ccc774a6bea1d081152f0732e9238f091013ed
Conjuction <suhinin-d@mail.ru>
2023-02-14 19:51:27 +0500

    add HW

--- src/test/java/Json/ParsingJson.java
+++ src/test/java/Json/ParsingJson.java
@@ -3,20 +3,31 @@ package Json;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ParsingJson {
+    ClassLoader cl = ParsingJson.class.getClassLoader();
+
     @Test
     void parseFile() throws IOException {
-        File file = new File ("src/test/resources/simple.json");
         ObjectMapper mp = new ObjectMapper();
-        PersonalData personalData = mp.readValue(file, PersonalData.class);
-        assertThat(personalData.firstName).isEqualTo("Rack");
-        assertThat(personalData.lastName).isEqualTo("Jackon");
-        assertThat(personalData.gender).isEqualTo("man");
-        assertThat(personalData.age).isEqualTo(24);
-
+        try (
+                InputStream resource = cl.getResourceAsStream("simple.json");
+                InputStreamReader reader = new InputStreamReader(resource);
+        ) {
+            PersonalData personalData = mp.readValue(reader, PersonalData.class);
+            assertThat(personalData.firstName).isEqualTo("Rack");
+            assertThat(personalData.lastName).isEqualTo("Jackon");
+            assertThat(personalData.gender).isEqualTo("man");
+            assertThat(personalData.age).isEqualTo(24);
+            assertThat(personalData.address.streetAddress).isEqualTo(126);
+            assertThat(personalData.address.city).isEqualTo("San Jone");
+            assertThat(personalData.address.state).isEqualTo("CA");
+            assertThat(personalData.address.postalCode).isEqualTo(394221);
+        }
     }
 }
--- src/test/java/Json/PersonalData.java
+++ src/test/java/Json/PersonalData.java
@@ -4,5 +4,13 @@ public class PersonalData {
     public String firstName;
     public String lastName;
     public String gender;
-    public int age;
+    public Integer age;
+    public Address address;
+
+    public static class Address {
+        public Integer streetAddress;
+        public String city;
+        public String state;
+        public Integer postalCode;
+    }
 }
